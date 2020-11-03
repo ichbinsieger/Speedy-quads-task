ba6ef90d349ddc468ef27f448ad7c3857138c7d5
sieger <oluwaseun.v@yahoo.com>
2020-11-03 23:19:27 +0100

    Speedy quads task

---
+++ SH.js
@@ -0,0 +1,11 @@
+function changecolor(color){
+    document.body.style.backgroundColor = color;
+}
+function bodycolor(color){
+    document.body.style.backgroundColor =color;
+}
+
+function colorwhite(color){
+    document.body.style.backgroundColor =color;
+    document.body.style.fontcolor ="#000000";
+}
